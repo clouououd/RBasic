601171ddb8d3cf2c9127c94dd83e38c39b1aa9ab
clouououd <wjdrhks02050@gmail.com>
2022-04-18 11:50:29 +0900

    220418

---
+++ 220418.R
@@ -0,0 +1,47 @@
+dim(available.packages())
+
+sessionInfo()
+
+install.packages("stringr")
+
+library(stringr)
+
+search() # 현재 로드된 패키지 확인
+
+remove.packages("stringr")
+
+data()
+
+.libPaths()
+
+hist(Nile)
+
+# 변수
+
+var1 <- 0
+var1
+var1 <- 1
+var1
+
+goods.code <- 'a001'  #문자값 저장
+goods.name <- '냉장고'
+
+age <- 35
+name <- "홍길동"
+age
+name
+
+age <- 35
+#컴바인 여러개를 변수에 입력하고자 할때
+names <- c('홍길동','이순신','유관순')
+
+.libPaths()
+.libPaths("C:/libTestR/Library")
+.libPaths()
+library(stringr)
+search()
+remove.packages("stringr")
+
+int 
+x <- is.numeric(int)
+x
